e49083919731a85708cdad0a84721fd1c595f572
joao.pires3@gmail.com <joao.pires3@gmail.com>
2023-08-24 17:01:11 -0300

    Criando metodos para iniciar, finalizar, atualizar tarefas

--- todolist/src/main/java/br/com/todolist/TodoController.java
+++ todolist/src/main/java/br/com/todolist/TodoController.java
@@ -41,4 +41,42 @@ public class TodoController {
             return ResponseEntity.noContent().build();
         }
     }
+
+    @PutMapping("/{todoId}/start_task")
+    public ResponseEntity<Todo> startTask(@PathVariable Integer todoId) {
+        Todo todoDatabase = this.todoRepo.findById(todoId).get();
+        if (todoDatabase != null) {
+            todoDatabase.setStatus(StatusEnum.IN_PROGRESS);
+            this.todoRepo.save(todoDatabase);
+            return ResponseEntity.ok(todoDatabase);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{todoId}/end_task")
+    public ResponseEntity<Todo> endTask(@PathVariable Integer todoId) {
+        Todo todoDatabase = this.todoRepo.findById(todoId).get();
+        if (todoDatabase != null) {
+            todoDatabase.setStatus(StatusEnum.FINISHED);
+            this.todoRepo.save(todoDatabase);
+            return ResponseEntity.ok(todoDatabase);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{todoId}")
+    public ResponseEntity<Todo> update(@PathVariable Integer todoId,
+                                       @RequestBody Todo todo) {
+        Todo todoDatabase = this.todoRepo.findById(todoId).get();
+        if (todoDatabase != null) {
+            todoDatabase.setTitle(todo.getTitle());
+            todoDatabase.setDescription(todo.getDescription());
+            this.todoRepo.save(todoDatabase);
+            return ResponseEntity.ok(todoDatabase);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
